9b7990bc0b36046b29cc18696ae8300cea678135
DiegoTO97 <ditoalpacaman@gmail.com>
2022-10-02 16:21:20 -0400

    full app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/public/login/login.component';
@@ -11,7 +11,6 @@ import { MenuComponent } from './components/public/menu/menu.component';
 
 //Servicios
 import { AuthService } from './services/auth.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthGuard } from './guard/auth.guard';
 
 @NgModule({
@@ -29,6 +28,8 @@ import { AuthGuard } from './guard/auth.guard';
     BrowserAnimationsModule
   ],
   providers: [AuthService, AuthGuard],
+  
   bootstrap: [AppComponent],
 })
+
 export class AppModule { }
--- src/app/components/private/home-page/home-page.component.ts
+++ src/app/components/private/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    this.router.navigate(['/menu']);
+    alert("logout");
   }
 
 }
